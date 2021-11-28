050888985857406c977920be797f3449ad630df8
Vjacheslav <syava_trader@ukr.net>
2021-11-28 22:47:50 +0200

    hw2

--- homework_2.js
+++ homework_2.js
@@ -1,9 +1,61 @@
-//Task 1
+// //Task 1
+//
+// let i=3;
+//
+// while (i){
+//     alert(i--); // 1 number will be shown last because next i=0, it's the same as false
+// }
 
-let i=3;
+// //Task 2
+//
+// let A = prompt("A=", "1");
+// let B = prompt("B=", "2");
+// let C = prompt("C=", "3");
+//
+// if (A < B && B < C) {
+//     A *= 2;
+//     B *= 2;
+//     C *= 2;
+//     console.log(A, B, C);
+// } else {
+//     A = -A;
+//     B = -B;
+//     C = -C;
+//     console.log(A, B, C);
+// }
 
-while (i){
-    alert(i--); // 1 number will be shown last because next i=0, it's the same as false
-}
+// //Task 3
+//
+// let A = prompt("A=", "1");
+// let B = prompt("B=", "2");
+// let C = prompt("C=", "3");
+//
+// if ((A < B && B < C)||(A > B && B > C)) {
+//     A *= 2;
+//     B *= 2;
+//     C *= 2;
+//     console.log(A, B, C);
+// } else {
+//     A = -A;
+//     B = -B;
+//     C = -C;
+//     console.log(A, B, C);
+// }
 
-//Task 2
+// //Task 4
+//
+// let A = prompt("A=", "2");   //2
+// let B = prompt("B=", "7");   //0
+// let C = prompt("C=", "-1");  //-1
+//
+// AB = B-A < 0 ? (B-A)*-1 : B-A;
+// AC = C-A < 0 ? (C-A)*-1 : C-A;
+//
+// if (AB < AC) {
+//     console.log(B, AB);
+// } else {
+//     console.log(C, AC);
+// }
+
+//Task 5
+
