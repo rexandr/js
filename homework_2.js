96690ed96c7a211359828ea33cff380d8444ffe0
Vjacheslav <syava_trader@ukr.net>
2021-11-29 23:09:07 +0200

    hw2

--- homework_2.js
+++ homework_2.js
@@ -57,5 +57,102 @@
 //     console.log(C, AC);
 // }
 
-//Task 5
+// //Task 5
+//
+// let X = prompt("X=", "2");
+// let Y = prompt("Y=", "7");
+//
+// if (X==0&&Y==0){
+//     console.log(0);
+// } else if(X!=0&&Y==0){
+//     console.log(1);
+// }else if(Y!=0&&X==0){
+//     console.log(2);
+// }else{
+//     console.log(3);
+// }
+
+// //Task 6
+//
+// let X = prompt("X=", "2");
+// let Y = prompt("Y=", "7");
+//
+// if (X>0&&Y>0){
+//     console.log("First aria");
+// }else if (X<0&&Y<0){
+//     console.log("Third aria");
+// }else if (X>0&&Y<0){
+//     console.log("Second aria");
+// }else if (X<0&&Y>0){
+//     console.log("Forth aria");
+// }else{
+//     console.log("The center of the world)))");
+// }
+
+// //Task 7?????????????????????????????????????????????????????????????????????????????????????????????????????????????
+//
+// let X1 = prompt("X1=", "1");
+// let Y1 = prompt("Y1=", "1");
+//
+// let X2 = prompt("X2=", "1");
+// let Y2 = prompt("Y2=", "4");
+//
+// let X3 = prompt("X3=", "5");
+// let Y3 = prompt("Y3=", "4");
+//
+// if (Y2 == Y3){
+//     console.log(X4=X3, Y4=Y1);
+// } else {
+//     console.log(X4=X2,Y4=Y2);
+// }
+//
+// //Task 8
+//
+// let year = prompt("Year =", "1999");
+//
+// if (year % 4 === 0 ) {
+//     console.log((year % 100 === 0 && year % 400 > 0)?"365":"366");
+// }else{
+//     console.log("365");
+// }
+
+// //Task 9
+//
+// let number = prompt("Number =", "-5");
+//
+// if (number > 0) {
+//     console.log("Positive");
+// } else if (number < 0) {
+//     console.log("Negative");
+// }else{
+//     console.log("Zero");
+// }
+
+// //Task 10
+//
+// let number = prompt("Number from 1 to 999 =", "111");
+//
+// x = number.length;
+// y = "Positive";
+//
+// if (number < 0){
+//    x--;
+//    y = "Negative";
+// }
+//
+//switch (x) {
+    // case 1 :
+    //     console.log(y, "Single");
+    //     break;
+    // case 2 :
+    //     console.log(y, "Double");
+    //     break;
+    // case 3 :
+    //     console.log(y, "Triple");
+    //     break;
+    // default :
+    //     console.log("Out of range");
+// }
+
+//Task 11
 
