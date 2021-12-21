9211af874fd17ca4c29bcba46d2dccdd941c53e1
Vjacheslav Golovach <syava_trader@ukr.net>
2021-12-21 23:10:04 +0200

    Projects

--- millionaire.js
+++ millionaire.js
@@ -1,76 +1,103 @@
-function start()
-{
+function start() {
 
     //правильну відповідь позачати, а при виводі позначку видаляти
     //при виводі питання видаляти його з масиву.
     //перепитати чи дісно використати підказку
 
+    //start button removing
     document.querySelector('#start').remove();
 
+
+    //friend calling hint button creating
     let friend = document.createElement("button");
     document.body.appendChild(friend);
-    friend.setAttribute('onclick', 'removeFriend()');
+    friend.setAttribute('onclick', 'document.querySelector(\'#friend\').remove()');
     friend.setAttribute('id', 'friend');
     friend.setAttribute('right', '20px');
     friend.innerText = 'Friend';
 
+    
+    //50/50 hint button creating
     let fifty = document.createElement("button");
     document.body.appendChild(fifty);
-    fifty.setAttribute('onclick', 'removeFifty()');
+    fifty.setAttribute('onclick', 'document.querySelector(\'#fifty\').remove()');
     fifty.setAttribute('id', 'fifty');
     fifty.innerText = '50/50';
 
-    let question = document.createElement("label");
-    document.body.appendChild(question);
-    question.setAttribute('id', 'lable');
-    question.innerText = arr[0][0];
+    showNextQuestion();
+
+}
 
 
+// Questions and answers array
+
+let arr =
+[
+    ['question 1', 'answer1', 'answer2@$', 'answer3', 'answer4'],
+    ['question 2', 'answer5', 'answer6@$', 'answer7', 'answer8'],
+    ['question 3', 'answer9', 'answer10@$', 'answer11', 'answer12'],
+    ['question 4', 'answer13', 'answer14@$', 'answer15', 'answer16'],
+    ['question 5', 'answer17', 'answer18@$', 'answer19', 'answer20'],
+]
+
+function showNextQuestion() {
+
+    //Question finding
+    let questionPosition = Math.random() * arr.length;
+    questionPosition = Math.floor(questionPosition);
+    console.log(questionPosition);
+
+    //Question showing
+    let shownQuestion = document.createElement("label");
+    document.body.appendChild(shownQuestion);
+    shownQuestion.setAttribute('id', 'lable');
+    shownQuestion.innerText = arr[+questionPosition][0];
+
+    //table with answers
     let table = document.querySelector('table');
 
-    let answer = 1;
+    //answers index
+    let answerIndex = 1;
 
+    
+    //delete previous answers from table
+    while (document.querySelector(".ans"))
+    {
+        document.querySelector(".ans").remove();
+    }
+
+    //right answer checking
+
+
+    
+    // table filling with answers
     for (let i = 0; i < table.rows.length; i++) {
         for (let j = 0; j < table.rows[i].cells.length; j++) {
             let button = document.createElement("button");
-            button.innerText = arr[0][answer];
-            answer++;
-            table.rows[i].cells[j].appendChild(button);;
+
+            let answer = arr[+questionPosition][answerIndex++];
+            let action = 'showNextQuestion()';
+            if (answer.indexOf('@$')>-1){                                       //find hint
+                answer = answer.slice(0,answer.indexOf('@$'));                  //cut hint
+            }else{
+                action = 'finishGame()';
+            }
+
+            button.innerText = answer;                                        //answer setting
+            button.setAttribute('onclick', action);
+            button.setAttribute('class', 'ans');
+            table.rows[i].cells[j].appendChild(button);
         }
     }
 
-
-
-
-    // let body = document.body;
-    // let table1 = document.createElement("table");
-    // body.appendChild(table1);
-    // let tr = document.createElement("tr");
-    // table1.appendChild(tr);
-    // table1.appendChild(tr);
-    // let td = document.createElement("td");
-    // let button = document.createElement("button");
-    // button.innerText = "Click";
-    // td.appendChild(button);
-    // tr.appendChild(td);
-
+    //delete shown question
+    arr.splice(questionPosition,1);
+    console.log(arr);
 
 }
 
-function removeFriend()
-{
-    document.querySelector('#friend').remove();
+
+//finishing game when user has wrong or press STOP button.
+function finishGame() {
+    alert('Game over!');
 }
-
-function removeFifty()
-{
-    document.querySelector('#fifty').remove();
-}
-
-
-let arr = [
-    ['question 1', 'answer1', 'answer2', 'answer3', 'answer4'],
-    ['question 2', 'answer5', 'answer6', 'answer7', 'answer8']
-            ]
-
-//delete question  after using      delete arr.question
