d9dbfb0bf69d958187a0a38750c0b7c02102c131
Vjacheslav Golovach <syava_trader@ukr.net>
2021-12-22 22:31:13 +0200

    prog

--- millionaire.js
+++ millionaire.js
@@ -1,52 +1,148 @@
 function start() {
 
-    //правильну відповідь позачати, а при виводі позначку видаляти
-    //при виводі питання видаляти його з масиву.
     //перепитати чи дісно використати підказку
     //if answer is right than increase a global variable and set price to another global value according to price-array's index.
+    //questions divide to three arrays in common array. Take questions according to difficulty from 1 then 2 then 3 array as separate array
+
 
     //start button removing
     document.querySelector('#start').remove();
 
 
+
     //friend calling hint button creating
     let friend = document.createElement("button");
     document.body.appendChild(friend);
-    friend.setAttribute('onclick', 'document.querySelector(\'#friend\').remove()');
+    friend.setAttribute('onclick', 'friend()');
     friend.setAttribute('id', 'friend');
     friend.setAttribute('right', '20px');
     friend.innerText = 'Friend';
 
-    
+    //stop game button creating
+    let stop = document.createElement("button");
+    document.body.appendChild(stop);
+    stop.setAttribute('onclick', 'finishGame(\'stop\')');
+    stop.setAttribute('id', 'stop');
+    stop.setAttribute('right', '20px');
+    stop.innerText = 'Stop game!';
+
     //50/50 hint button creating
     let fifty = document.createElement("button");
     document.body.appendChild(fifty);
-    fifty.setAttribute('onclick', 'document.querySelector(\'#fifty\').remove()');
+    fifty.setAttribute('onclick', 'fiftyFifty()');
     fifty.setAttribute('id', 'fifty');
     fifty.innerText = '50/50';
 
-    showNextQuestion();
+    showNextQuestion(level=0);
 
 }
 
 
 // Questions and answers array
 
-let arr =
+let answersAll =
 [
-    ['question 1', 'answer1', 'answer2@$', 'answer3', 'answer4'],
-    ['question 2', 'answer5', 'answer6@$', 'answer7', 'answer8'],
-    ['question 3', 'answer9', 'answer10@$', 'answer11', 'answer12'],
-    ['question 4', 'answer13', 'answer14@$', 'answer15', 'answer16'],
-    ['question 5', 'answer17', 'answer18@$', 'answer19', 'answer20'],
+    [
+        ['question 1', 'answer1', 'answer2@$', 'answer3', 'answer4'],
+        ['question 2', 'answer5', 'answer6@$', 'answer7', 'answer8'],
+        ['question 3', 'answer9', 'answer10@$', 'answer11', 'answer12'],
+        ['question 4', 'answer13', 'answer14@$', 'answer15', 'answer16'],
+        ['question 5', 'answer17', 'answer18@$', 'answer19', 'answer20'],
+        ['question 6', 'answer21', 'answer22@$', 'answer23', 'answer24'],
+        ['question 7', 'answer25', 'answer26@$', 'answer27', 'answer28'],
+    ],
+    [
+        ['question 8', 'answer1', 'answer2@$', 'answer3', 'answer4'],
+        ['question 9', 'answer5', 'answer6@$', 'answer7', 'answer8'],
+        ['question 10', 'answer9', 'answer10@$', 'answer11', 'answer12'],
+        ['question 11', 'answer13', 'answer14@$', 'answer15', 'answer16'],
+        ['question 12', 'answer17', 'answer18@$', 'answer19', 'answer20'],
+        ['question 13', 'answer21', 'answer22@$', 'answer23', 'answer24'],
+        ['question 14', 'answer25', 'answer26@$', 'answer27', 'answer28'],
+    ],
+    [
+        ['question 15', 'answer1', 'answer2@$', 'answer3', 'answer4'],
+        ['question 16', 'answer5', 'answer6@$', 'answer7', 'answer8'],
+        ['question 17', 'answer9', 'answer10@$', 'answer11', 'answer12'],
+        ['question 18', 'answer13', 'answer14@$', 'answer15', 'answer16'],
+        ['question 19', 'answer17', 'answer18@$', 'answer19', 'answer20'],
+        ['question 20', 'answer21', 'answer22@$', 'answer23', 'answer24'],
+        ['question 21', 'answer25', 'answer26@$', 'answer27', 'answer28'],
+    ],
 ]
 
-function showNextQuestion() {
+//answer's prices
+let prices =
+    [
+        100,
+        200,
+        300,
+        500,
+        1000,
+        2000,
+        4000,
+        8000,
+        16000,
+        32000,
+        64000,
+        125000,
+        250000,
+        500000,
+        1000000
+    ]
+
+let userWin = 0;    //incombustible sum
+
+let k = 0;          //current price's position
+
+
+
+function showNextQuestion(level) {
+
+
+
+    //level indication
+    if (level === 1) {
+
+        let message = `You are right. Your win - ${prices[k]} Ready to the next question?`;
+
+
+        if (k === 4 || k === 9 || k === 14){
+            userWin = prices[k];
+            console.log(userWin);
+            
+            message = `You are right. Your win - ${prices[k]} and it is incombustible sum. Ready to the next question?`;
+        }
+
+        alert(message);
+
+        k++;
+
+    }
+
+
+    //question's difficulties changing
+    let arr = [];
+    if (k<=4){
+
+        arr = answersAll[0];
+
+    }else if (k>4 && k<=9){
+
+        arr = answersAll[1];
+
+    }else{
+
+        arr = answersAll[2];
+
+    }
+
+
 
     //Question finding
     let questionPosition = Math.random() * arr.length;
     questionPosition = Math.floor(questionPosition);
-    console.log(questionPosition);
+    //console.log(questionPosition);
 
     //Question showing
     let shownQuestion = document.createElement("label");
@@ -62,43 +158,80 @@ function showNextQuestion() {
 
     
     //delete previous answers from table
-    while (document.querySelector(".ans"))
+    while (document.querySelector(".wrong"))
     {
-        document.querySelector(".ans").remove();
+        document.querySelector(".wrong").remove();
     }
+    document.querySelector(".right")?document.querySelector(".right").remove():'';
 
     //right answer checking
 
 
     
     // table filling with answers
+    let ansNum = 1;                                                   //answer's numerating
     for (let i = 0; i < table.rows.length; i++) {
         for (let j = 0; j < table.rows[i].cells.length; j++) {
             let button = document.createElement("button");
-
             let answer = arr[+questionPosition][answerIndex++];
-            let action = 'showNextQuestion()';
+            let action = 'showNextQuestion(level=1)';
+            let classAt = 'right';
             if (answer.indexOf('@$')>-1){                                       //find hint
                 answer = answer.slice(0,answer.indexOf('@$'));                  //cut hint
             }else{
                 action = 'finishGame()';
+                classAt = 'wrong';
             }
-
-            button.innerText = answer;                                        //answer setting
+            button.innerText = ansNum + answer;                                        //answer setting
             button.setAttribute('onclick', action);
-            button.setAttribute('class', 'ans');
+            button.setAttribute('class', classAt);
             table.rows[i].cells[j].appendChild(button);
+            ansNum++;
         }
     }
 
+
+    k === 15 ? finishGame('win') : '';      //last question is answered
+
     //delete shown question
     arr.splice(questionPosition,1);
-    console.log(arr);
+    //console.log(arr);
 
 }
 
+//hint from friend
+function friend() {
 
-//finishing game when user has wrong or press STOP button.
-function finishGame() {
-    alert('Game over!');
+    if (confirm("Are you sure?"))
+    {
+        let ran = 1 + Math.random() * 4;
+        ran = Math.floor(ran);
+        alert(`I guess the answer is ${ran}`);
+        document.querySelector('#friend').remove();
+    }
+
+}
+
+function fiftyFifty() {
+    document.querySelector('.wrong').remove();
+    document.querySelector('.wrong');
+    document.querySelector('.wrong').remove();
+
+    document.querySelector('#fifty').remove();
+}
+
+//finishing game when user wrong, stopped game or won the game.
+function finishGame(val) {
+
+    if (val === 'stop'){
+        alert(`Game over! Your win is ${prices[k-1]}`);
+        location.reload();
+    } else if(val === 'win') {
+        alert('You are the WINNER!');
+        location.reload();
+    }else{
+        alert(`Game over! Your win is ${userWin}`);
+        location.reload();
+    }
+
 }
