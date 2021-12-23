8693e101b230086b27dede0145647f51c80f499d
Vjacheslav Golovach <syava_trader@ukr.net>
2021-12-23 22:58:17 +0200

    Millionair improved

--- millionaire.js
+++ millionaire.js
@@ -9,7 +9,6 @@ function start() {
     document.querySelector('#start').remove();
 
 
-
     //friend calling hint button creating
     let friend = document.createElement("button");
     document.body.appendChild(friend);
@@ -33,7 +32,7 @@ function start() {
     fifty.setAttribute('id', 'fifty');
     fifty.innerText = '50/50';
 
-    showNextQuestion(level=0);
+    showNextQuestion(level = 0);
 
 }
 
@@ -41,35 +40,35 @@ function start() {
 // Questions and answers array
 
 let answersAll =
-[
     [
-        ['Как называется наш предмет?', 'Java', 'JavaScript@$', 'Delphi', 'C#'],
-        ['Как зовут преподавателя?', 'Костянтин@$', 'Василий', 'Петрович', 'Сам приходит'],
-        ['Как фамилия преподавателя?', 'Гейтс', 'Маккалистер', 'Онищенко@$', 'Сусанин'],
-        ['Какого цвета книга по предмету?', 'Заюзаного', 'Затёртого', 'Серо-буро-малинового', 'Неизвестно@$'],
-        ['Сколько было лекций?', '3', '5@$', '20', '100'],
-        ['Предмет был интересный?', 'Нет', '50/50', 'Вы о чём?', 'Да@$'],
-        ['Какого числа защита проэктов?', 'Её нет', '24@$', '31', '1'],
-    ],
-    [
-        ['Где ежегодно проводят день открытых дверей?', 'Дома', 'В вузе@$', 'На роботе', 'В Египте'],
-        ['Чего не было на Диме Билане во время победного выступления на «Евровидении-2008»?', 'обуви@$', 'носков', 'рубашки', 'цепочки'],
-        ['Кто регулярно исполняет хаку, ритуальный танец народа маори?', 'сёрфингисты Австралии', 'регбисты Новой Зеландии@$', 'лесорубы США', 'Джуны'],
-        ['Что, как считается, кричат танцующие лезгинку?', 'Ура', 'Свободная касса', 'Асса@$', 'Вася'],
-        ['В какой стране был опубликован первый в мире прогноз погоды?', 'Украина', 'Германия', 'Франция', 'Великобритания@$'],
-        ['Какое море фигурирует в названии фильмов о Джеке Воробье?', 'Баренцево', 'Мертвое', 'Карибское@$', 'Черное'],
-        ['Из книги какого писателя иллюзионист Дэвид Копперфильд взял себе псевдоним?', 'Джек Лондон', 'Чарльз Диккенс@$', 'Оскар Уайльд', 'Эдгар По'],
-    ],
-    [
-        ['Какую майку носит «горный король» гонки «Тур де Франс»?', 'клетчатую', 'в горошек@$', 'полосатую', 'радужную'],
-        ['Какой углеводород является основным компонентом природного газа?', 'гептан', 'пропан', 'метан@$', 'бутан'],
-        ['В каком кинофильме суд над главным героем был назначен на 18 часов 27 июля?', 'Бриллиантовая рука@$', 'Кавказская пленница', 'Берегись автомобиля', 'Мимино'],
-        ['Как в Италии называют холодный северный ветер, приходящий с Альпийских гор?', 'памперо', 'сирокко', 'мистраль', 'трамонтана@$'],
-        ['Где можно найти пятно Ктулху и бороду Слейпнир?', 'на Нептуне', 'на Плутоне@$', 'на Уране', 'на Сатурне'],
-        ['Из чего индейцы Гвианы, охотившиеся в бассейне Амазонки, изготавливали яд кураре?', 'из коры растений@$', 'из желез змей', 'из пластинчатых грибов', 'из слюны тёщи'],
-        ['От слова с каким значением произошло название «трико»?', 'тонкий', 'вязаный@$', 'темный', 'опасный'],
-    ],
-]
+        [
+            ['Как называется наш предмет?', 'Java', 'JavaScript@$', 'Delphi', 'C#'],
+            ['Как зовут преподавателя?', 'Костянтин@$', 'Василий', 'Петрович', 'Сам приходит'],
+            ['Как фамилия преподавателя?', 'Гейтс', 'Маккалистер', 'Онищенко@$', 'Сусанин'],
+            ['Какого цвета книга по предмету?', 'Заюзаного', 'Затёртого', 'Серо-буро-малинового', 'Неизвестно@$'],
+            ['Сколько было лекций?', '3', '5@$', '20', '100'],
+            ['Предмет был интересный?', 'Нет', '50/50', 'Вы о чём?', 'Да@$'],
+            ['Какого числа защита проэктов?', 'Её нет', '24@$', '31', '1'],
+        ],
+        [
+            ['Где ежегодно проводят день открытых дверей?', 'Дома', 'В вузе@$', 'На роботе', 'В Египте'],
+            ['Чего не было на Диме Билане во время победного выступления на «Евровидении-2008»?', 'обуви@$', 'носков', 'рубашки', 'цепочки'],
+            ['Кто регулярно исполняет хаку, ритуальный танец народа маори?', 'сёрфингисты Австралии', 'регбисты Новой Зеландии@$', 'лесорубы США', 'Джуны'],
+            ['Что, как считается, кричат танцующие лезгинку?', 'Ура', 'Свободная касса', 'Асса@$', 'Вася'],
+            ['В какой стране был опубликован первый в мире прогноз погоды?', 'Украина', 'Германия', 'Франция', 'Великобритания@$'],
+            ['Какое море фигурирует в названии фильмов о Джеке Воробье?', 'Баренцево', 'Мертвое', 'Карибское@$', 'Черное'],
+            ['Из книги какого писателя иллюзионист Дэвид Копперфильд взял себе псевдоним?', 'Джек Лондон', 'Чарльз Диккенс@$', 'Оскар Уайльд', 'Эдгар По'],
+        ],
+        [
+            ['Какую майку носит «горный король» гонки «Тур де Франс»?', 'клетчатую', 'в горошек@$', 'полосатую', 'радужную'],
+            ['Какой углеводород является основным компонентом природного газа?', 'гептан', 'пропан', 'метан@$', 'бутан'],
+            ['В каком кинофильме суд над главным героем был назначен на 18 часов 27 июля?', 'Бриллиантовая рука@$', 'Кавказская пленница', 'Берегись автомобиля', 'Мимино'],
+            ['Как в Италии называют холодный северный ветер, приходящий с Альпийских гор?', 'памперо', 'сирокко', 'мистраль', 'трамонтана@$'],
+            ['Где можно найти пятно Ктулху и бороду Слейпнир?', 'на Нептуне', 'на Плутоне@$', 'на Уране', 'на Сатурне'],
+            ['Из чего индейцы Гвианы, охотившиеся в бассейне Амазонки, изготавливали яд кураре?', 'из коры растений@$', 'из желез змей', 'из пластинчатых грибов', 'из слюны тёщи'],
+            ['От слова с каким значением произошло название «трико»?', 'тонкий', 'вязаный@$', 'темный', 'опасный'],
+        ],
+    ]
 
 //answer's prices
 let prices =
@@ -96,7 +95,6 @@ let userWin = 0;    //incombustible sum
 let k = 0;          //current price's position
 
 
-
 function showNextQuestion(level) {
 
 
@@ -106,11 +104,10 @@ function showNextQuestion(level) {
 
         let message = `You are right. Your win - ${prices[k]} Ready to the next question?`;
 
-
-        if (k === 4 || k === 9 || k === 14){
+        if (k === 4 || k === 9 || k === 14) {
             userWin = prices[k];
             console.log(userWin);
-            
+
             message = `You are right. Your win - ${prices[k]} and it is incombustible sum. Ready to the next question?`;
         }
 
@@ -120,23 +117,21 @@ function showNextQuestion(level) {
 
     }
 
-
     //question's difficulties changing
     let arr = [];
-    if (k<=4){
+    if (k <= 4) {
 
         arr = answersAll[0];
 
-    }else if (k>4 && k<=9){
+    } else if (k > 4 && k <= 9) {
 
         arr = answersAll[1];
 
-    }else{
+    } else {
 
         arr = answersAll[2];
 
     }
-
 
 
     //Question finding
@@ -156,18 +151,15 @@ function showNextQuestion(level) {
     //answers index
     let answerIndex = 1;
 
-    
+
     //delete previous answers from table
-    while (document.querySelector(".wrong"))
-    {
+    while (document.querySelector(".wrong")) {
         document.querySelector(".wrong").remove();
     }
-    document.querySelector(".right")?document.querySelector(".right").remove():'';
+    document.querySelector(".right") ? document.querySelector(".right").remove() : '';
 
     //right answer checking
 
-
-    
     // table filling with answers
     let ansNum = 1;                                                   //answer's numerating
     for (let i = 0; i < table.rows.length; i++) {
@@ -176,9 +168,9 @@ function showNextQuestion(level) {
             let answer = arr[+questionPosition][answerIndex++];
             let action = 'showNextQuestion(level=1)';
             let classAt = 'right';
-            if (answer.indexOf('@$')>-1){                                       //find hint
-                answer = answer.slice(0,answer.indexOf('@$'));                  //cut hint
-            }else{
+            if (answer.indexOf('@$') > -1) {                                       //find hint
+                answer = answer.slice(0, answer.indexOf('@$'));                  //cut hint
+            } else {
                 action = 'finishGame()';
                 classAt = 'wrong';
             }
@@ -194,7 +186,7 @@ function showNextQuestion(level) {
     k === 15 ? finishGame('win') : '';      //last question is answered
 
     //delete shown question
-    arr.splice(questionPosition,1);
+    arr.splice(questionPosition, 1);
     //console.log(arr);
 
 }
@@ -202,8 +194,7 @@ function showNextQuestion(level) {
 //hint from friend
 function friend() {
 
-    if (confirm("Are you sure?"))
-    {
+    if (confirm("Are you sure?")) {
         let ran = 1 + Math.random() * 4;
         ran = Math.floor(ran);
         alert(`I guess the answer is ${ran}`);
@@ -213,24 +204,26 @@ function friend() {
 }
 
 function fiftyFifty() {
-    document.querySelector('.wrong').remove();
-    document.querySelector('.wrong');
-    document.querySelector('.wrong').remove();
 
-    document.querySelector('#fifty').remove();
+    if (confirm("Are you sure?")) {
+        document.querySelector('.wrong').remove();
+        document.querySelector('.wrong');
+        document.querySelector('.wrong').remove();
+        document.querySelector('#fifty').remove();
+    }
 }
 
 //finishing game when user wrong, stopped game or won the game.
 function finishGame(val) {
 
-    if (val === 'stop'){
-        alert(`Game over! Your win is ${prices[k-1]||0}`);
+    if (val === 'stop') {
+        alert(`Game over! Your win is ${prices[k - 1] || 0}`);
         location.reload();
-    } else if(val === 'win') {
+    } else if (val === 'win') {
         alert('You are the WINNER!');
         location.reload();
-    }else{
-        alert(`Game over! Your win is ${userWin||0}`);
+    } else {
+        alert(`Game over! Your win is ${userWin || 0}`);
         location.reload();
     }
 
